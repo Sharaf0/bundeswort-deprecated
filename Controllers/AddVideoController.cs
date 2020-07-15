2ac91f128ba1f61118f7a6732d1f60811bef7031
Mohammad Sharaf <mohammedsharaf.1992@gmail.com>
2020-07-16 00:23:48 +0200

    Added Redis Caching

--- Controllers/AddVideoController.cs
+++ Controllers/AddVideoController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Bundeswort.Scraper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Logging;
 
 namespace Bundeswort.Controllers
@@ -13,18 +15,32 @@ namespace Bundeswort.Controllers
     public class AddVideoController : ControllerBase
     {
         private readonly ILogger<AddVideoController> _logger;
+        private readonly IDistributedCache distributedCache;
 
-        public AddVideoController(ILogger<AddVideoController> logger)
+        public AddVideoController(ILogger<AddVideoController> logger, IDistributedCache distributedCache)
         {
-            _logger = logger;
+            this._logger = logger;
+            this.distributedCache = distributedCache;
         }
 
         [HttpPost]
         public async Task<int> AddVideo([FromBody] VideoDetails videoDetails)
         {
-            CaptionsScraper scraper = new CaptionsScraper(new Client());
-            var res = await scraper.Scrap(videoDetails.VideoId, new string[] { videoDetails.Language.ToLower() });
-            return res.Count;
+            var cached = await distributedCache.GetAsync(videoDetails.VideoId);
+            if(cached == null)
+            {
+                CaptionsScraper scraper = new CaptionsScraper(new Client());
+                var res = await scraper.Scrap(videoDetails.VideoId, new string[] { videoDetails.Language.ToLower() });
+                var options = new DistributedCacheEntryOptions()
+                .SetSlidingExpiration(TimeSpan.FromDays(1))
+                .SetAbsoluteExpiration(TimeSpan.FromDays(6));
+                await distributedCache.SetAsync(videoDetails.VideoId, Encoding.UTF8.GetBytes("true"), options);
+                return res.Count;
+            }
+            else
+            {
+                return 0;
+            }
         }
     }
 
--- Startup.cs
+++ Startup.cs
@@ -28,6 +28,9 @@ namespace Bundeswort
             {
                 configuration.RootPath = "ClientApp/build";
             });
+
+            services.AddMemoryCache();
+            services.AddStackExchangeRedisCache(options => options.Configuration = "localhost:6379");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
