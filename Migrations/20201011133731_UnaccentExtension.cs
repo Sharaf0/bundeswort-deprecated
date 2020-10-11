b838690ed7340b10d0205f79d0f8b8040f6cf531
Mohammad Sharaf <mohammedsharaf.1992@gmail.com>
2020-10-11 15:39:44 +0200

    Unaccent Extension Migration

---
+++ Migrations/20201011133731_UnaccentExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bundeswort.Migrations
+{
+    public partial class UnaccentExtension : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"CREATE EXTENSION unaccent");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"DROP EXTENSION unaccent");
+        }
+    }
+}
